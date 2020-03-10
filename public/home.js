fc57bec1bbed897e6e237e179aaf98c123196f9a
cairnsi <59540148+cairnsi@users.noreply.github.com>
2020-03-09 21:17:18 -0500

    afa

--- public/home.js
+++ public/home.js
@@ -1,2 +1,23 @@
 
 
+function bindButtons(){
+  document.getElementById('insertSubmit').addEventListener('click', function(event){
+	  var req = new XMLHttpRequest();
+	  var payload = {name:null};
+	  payload.name = document.getElementById('name').value;
+	  req.open('POST', '/tableInsert', true);
+	  req.setRequestHeader('Content-Type', 'application/json');
+	  req.addEventListener('load',function(){
+	  if(req.status >= 200 && req.status < 400){
+		var response = JSON.parse(req.responseText);
+		
+	  } else {
+		console.log("Error in network request: " + req.statusText);
+	  }});
+	  req.send(JSON.stringify(payload));
+	  event.preventDefault();
+  });
+}
+
+document.addEventListener('DOMContentLoaded', bindButtons);
+
