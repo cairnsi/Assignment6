8ef847e6ffef0cb6b5b767388936ade22abb0732
cairnsi <59540148+cairnsi@users.noreply.github.com>
2020-03-09 21:26:21 -0500

    afa

--- Assignnemt6Handlebars.js
+++ Assignnemt6Handlebars.js
@@ -20,7 +20,7 @@ var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
   user            : 'cs290_cairnsi',
-  password        : '0826',
+  password        : credentials.databasePWD,
   database        : 'cs290_cairnsi'
 });
 
