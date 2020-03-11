002aec9c2254a846f4ae70b10bee76ae1753179a
cairnsi <59540148+cairnsi@users.noreply.github.com>
2020-03-10 19:40:46 -0500

    afaf

--- public/home.js
+++ public/home.js
@@ -5,18 +5,32 @@ function clearMessages(){
 		 messages[i].textContent="";
 	 }
 }
+
+function displayTable(){
+	
+}
 function bindButtons(){
   document.getElementById('insertSubmit').addEventListener('click', function(event){
 	  var req = new XMLHttpRequest();
 	  var payload = {name:null};
+	  clearMessages();
+	  if(document.getElementById('name').value==""){
+		 document.getElementById('addMessage').textContent="Name cannot be empty";
+		 document.getElementById('addMessage').style.color='red';
+		 return;
+	  }
 	  payload.name = document.getElementById('name').value;
+	  payload.reps = document.getElementById('reps').value;
+	  payload.weight = document.getElementById('weight').value;
+	  payload.unit = document.getElementsByName('unit').value;
+	  payload.date = document.getElementById('date').value;
 	  req.open('POST', '/tableInsert', true);
 	  req.setRequestHeader('Content-Type', 'application/json');
 	  req.addEventListener('load',function(){
-	  clearMessages();
 	  if(req.status >= 200 && req.status < 400){
 		var response = JSON.parse(req.responseText);
 		 document.getElementById('name').value="";
+		 document.getElementById('reps').reps="";
 		 document.getElementById('addMessage').textContent="Successful Insert";
 		 document.getElementById('addMessage').style.color='green';
 		
